b9b480cc6627febe85d10b32d5a6a29ae08845fc
limonada143 <eunhye143@gmail.com>
2025-06-21 14:12:00 +0900

    test #1

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const shoesData = [
+  {
+    brand: "Nike",
+    model: "ZoomX Vaporfly Next%",
+    drop: 8,
+    weight: 190,
+    cushion: "High",
+    price: 299000,
+    purpose: "레이스",
+    img: "images/nike_vaporfly.jpg"
+  },
+  {
+    brand: "Asics",
+    model: "Gel Nimbus 26",
+    drop: 10,
+    weight: 290,
+    cushion: "Very High",
+    price: 189000,
+    purpose: "일반 러닝",
+    img: "images/asics_nimbus.jpg"
+  }
+];
